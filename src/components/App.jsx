9d5c85c88967acacd481601b3dbbc4f6d7025443
Yevhen Vasylevytskyi <e.vasylevytskyi@gmail.com>
2023-04-09 14:38:29 +0300

    add filter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,13 @@ export class App extends Component {
     super();
 
     this.state = {
-      contacts: [],
+      contacts: [
+        {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+        {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+        {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+        {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+      ],
+      filter: '',
       name: '',
       number: ''
     };
@@ -31,8 +37,24 @@ export class App extends Component {
     form.reset();
   };
 
+  handleChange = evt => {
+    this.setState({ filter: evt.target.value });
+  };
+
+  
+
   render() {   
-    const {contacts} = this.state
+    const { contacts } = this.state;
+    const { filter } = this.state;
+    const normalizeFilterInput = filter.toLocaleLowerCase()
+
+    const filteredNameContact = contacts.filter(contact => {
+      const normalizeName = contact.name.toLocaleLowerCase()
+
+      if (normalizeName.includes(normalizeFilterInput)) {
+        return contact
+      }
+    })
 
     return (      
       <>
@@ -62,8 +84,10 @@ export class App extends Component {
         </form>
 
         <h2>Conatcts</h2>
+        <p>Find contacts by name</p>
+        <input type="text" value={filter} onChange={this.handleChange} />
         <ul>
-          {contacts.map(contact => (
+          {filteredNameContact.map(contact => (
             <li key={contact.id}>{contact.name}: {contact.number}</li>
           ))}
         </ul>
