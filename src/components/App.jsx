f696a4bba2e910b2ad501793431f480658ef14c2
Yevhen Vasylevytskyi <e.vasylevytskyi@gmail.com>
2023-04-09 14:11:43 +0300

    add tell

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import { Component } from "react";
-import { nanoid } from 'nanoid';
+import { nanoid } from "nanoid";
+import s from "./App.module.css";
 
 export class App extends Component {
   constructor() {
@@ -7,7 +8,8 @@ export class App extends Component {
 
     this.state = {
       contacts: [],
-      name: ''
+      name: '',
+      number: ''
     };
   }
 
@@ -15,11 +17,13 @@ export class App extends Component {
     evt.preventDefault();
     const form = evt.currentTarget;
     const name = form.elements.name.value;
+    const number = form.elements.number.value;
     const fomData = {
       id: nanoid(),
-      name: name
+      name: name,
+      number: number
     }
-
+    console.log(fomData)
     this.setState(prevState => {
       return { contacts: [...prevState.contacts, fomData] }
     });
@@ -34,20 +38,33 @@ export class App extends Component {
       <>
         <h1>Phonebook</h1>
         <form onSubmit={this.handleSubmit}>
-        <input
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-        />
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+          <label>
+            Number
+            <input
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+                />
+          </label>
         <button type="submit">Add contact</button>
         </form>
 
         <h2>Conatcts</h2>
         <ul>
           {contacts.map(contact => (
-            <li key={contact.id}>{contact.name}</li>
+            <li key={contact.id}>{contact.name}: {contact.number}</li>
           ))}
         </ul>
       </>        
