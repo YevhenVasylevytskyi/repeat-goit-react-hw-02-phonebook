1f124cc75582041b6080a4ace05845d7ed4c3204
Yevhen Vasylevytskyi <e.vasylevytskyi@gmail.com>
2023-04-11 08:15:25 +0300

    replace ContactForm

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import { nanoid } from "nanoid";
-import s from "./App.module.css";
+// import s from "./App.module.css";
+import ContactForm from "./ContactForm/ContactForm";
 
 export class App extends Component {
   constructor() {
@@ -59,29 +60,10 @@ export class App extends Component {
     return (      
       <>
         <h1>Phonebook</h1>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Name
-            <input
-              type="text"
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-            />
-          </label>
-          <label>
-            Number
-            <input
-              type="tel"
-              name="number"
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
-                />
-          </label>
-        <button type="submit">Add contact</button>
-        </form>
+
+        <ContactForm
+          handleSubmit={this.handleSubmit}
+        />        
 
         <h2>Conatcts</h2>
         <p>Find contacts by name</p>
